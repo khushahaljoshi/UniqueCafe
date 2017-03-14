f9ba6a8652da0ca07f610311cc7cb4529eebd0ce
joshi1996 <khushahaljoshi111@gmail.com>
2017-03-14 13:42:23 +0530

    Initial commit

---
+++ app/src/main/java/com/example/kjoshi/uniquecafe/MainActivity.java
@@ -0,0 +1,14 @@
+
+package com.example.kjoshi.uniquecafe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
